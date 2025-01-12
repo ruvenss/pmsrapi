f06bff51c548130d075b6df2d37872f13bb6f1bb
Ruvenss G. Wilches <ruvenss@gmail.com>
2025-01-12 12:17:12 +0100

    update

--- v1/index.php
+++ v1/index.php
@@ -3,6 +3,7 @@ include_once 'config.php';
 if (file_exists(config_path)) {
     define("ms_secrets", json_decode(file_get_contents(config_path), true));
     define("ms_logserver_token", ms_secrets['logserver_token']);
+    define("ms_environment", ms_secrets['env']);
 } else {
     define("ms_secrets", []);
     http_response(500, ["error" => "Configuration file not found"]);
@@ -56,22 +57,45 @@ function http_response($http_code = 200, $data = null)
  * @param string $log_for2 = if is not defined the MicroService Name will be used
  * @param int $log_for_id2 = if is not defined 0 will be used  (max 30 characters)
  * @param int $deleted = 0 or 1 if the record was deleted
- * @return string
+ * @return boolean
  */
-function log_event($changes = null, $action = "updated", $log_type = "task", $created_by = 1, $log_type_title = ms_name, $log_type_id = 1, $log_for = ms_name, $log_for_id = 0, $log_for2 = ms_name, $log_for_id2 = null, $deleted = 0)
+function log_event($changes, $env = "dev", $action = "updated", $log_type = "task", $created_by = 1, $log_type_title = ms_name, $log_type_id = 1, $log_for = ms_name, $log_for_id = 0, $log_for2 = ms_name, $log_for_id2 = null, $deleted = 0)
 {
-    $logserver = ms_logserver;
-    $token = ms_logserver_token;
-    $data = ["microservice" => ms_name, "version" => ms_version, "action" => $action];
-    $options = [
-        'http' => [
-            'header' => "Content-type: application/json\r\n" .
-                "Authorization: Bearer $token\r\n",
-            'method' => 'POST',
-            'content' => json_encode($data)
-        ]
-    ];
-    $context = stream_context_create($options);
-    $result = file_get_contents($logserver, false, $context);
-    return $result;
+    if ($env == ms_environment) {
+        $logserver = ms_logserver;
+        $token = ms_logserver_token;
+        $data = ["microservice" => ms_name, "version" => ms_version, "created_at" => date("Y-m-d H:i:s"), "changes" => $changes, "action" => $action, "log_type" => $log_type, "created_by" => $created_by, "log_type_title" => $log_type_title, "log_type_id" => $log_type_id, "log_for" => $log_for, "log_for_id" => $log_for_id, "log_for2" => $log_for2, "log_for_id2" => $log_for_id2, "deleted" => $deleted];
+        // There is 2 type of log we can send to the log server or we can save it in the local file
+        if ($logserver == null || $logserver == "") {
+            // Save the log in the local file
+            file_put_contents(config_path['local_log']['path'], json_encode($data) . PHP_EOL, FILE_APPEND);
+            return true;
+        }
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => ms_logserver,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => json_encode($data),
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json',
+                'Authorization: Bearer ' . ms_logserver_token
+            ),
+        ));
+        curl_exec($curl);
+        if (curl_errno($curl)) {
+            curl_close($curl);
+            return false;
+        } else {
+            curl_close($curl);
+            return true;
+        }
+    } else {
+        return false;
+    }
 }
