839978555c73636e8a1f1d41b348df2cb6d12e0b
Ruvenss G. Wilches <ruvenss@gmail.com>
2025-01-26 19:01:48 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ CURRENT_DIR=$(pwd)
 PARENT_DIR=$(dirname "$CURRENT_DIR")
 SAMPLE_CONFIG="${CURRENT_DIR}/v1/sample_config.json"
 # Define the cron job command
-CRON_JOB="0 * * * * cd $CURRENT_DIR;php update.php"
+CRON_JOB="0 * * * * cd $CURRENT_DIR;php autoupdate.php"
 # Check if the cron job already exists
 CRON_EXISTS=$(crontab -l 2>/dev/null | grep -F "$CRON_JOB")
 platform='unknown'
@@ -30,7 +30,7 @@ if [[ "$unamestr" == 'Linux' ]]; then
     if dialog --title 'Deployment directory' --backtitle "PMSRAPI" --yesno "Confirm deployment directory:\n$CURRENT_DIR\n\nConfirm parent directory:\n$PARENT_DIR" 17 60; then
         dialog --title 'Config' --infobox '✅ Let us start by setting the basic config file' 7 60
         sleep 3
-        mms_name=$(dialog --backtitle "PMSRAPI " --title 'config.php' --inputbox 'Enter your microservice name' 7 60  --output-fd 1)
+        mms_name=$(dialog --backtitle "PMSRAPI" --title 'config.php' --inputbox 'Enter your microservice name' 7 60  --output-fd 1)
         mms_version=$(dialog --title 'PMSRAPI' --inputbox 'Enter your microservice version' 7 60 --output-fd 1)
         mms_description=$(dialog --title 'PMSRAPI' --inputbox 'Enter your microservice Description' 7 60  --output-fd 1)
         mms_author=$(dialog --title 'PMSRAPI' --inputbox 'Enter your microservice main author name' 7 60  --output-fd 1)
@@ -40,6 +40,7 @@ if [[ "$unamestr" == 'Linux' ]]; then
         mms_last_updated=$(dialog --title 'PMSRAPI' --inputbox 'Last update date' 7 60  --output-fd 1)
         mms_github_repo=$(dialog --title 'PMSRAPI' --inputbox 'GitHub Repo' 7 60  --output-fd 1)
         mms_logserver=$(dialog --title 'PMSRAPI' --inputbox 'Logness Server URL' 7 60  --output-fd 1)
+        mms_logservertoken=$(dialog --title 'PMSRAPI' --inputbox 'Logness Token' 7 60  --output-fd 1)
         mms_config_path="${PARENT_DIR}/${mms_name}.json"
         CONFIG_DATA="<?php\n
 // Define the database connection and private tokens out of your source code\n
@@ -62,7 +63,76 @@ define(\"ms_logserver\", \"${mms_logserver}\");
 "
         echo "$CONFIG_DATA" > $CURRENT_DIR/v1/config.php
         chown -R www-data:www-data $CURRENT_DIR
-        mv $SAMPLE_CONFIG $mms_config_path
+        httpport=$(dialog --backtitle "PMSRAPI" --title 'MicroService HTTP' --inputbox 'Enter the port where this service will live' 7 60 "8001"  --output-fd 1)
+        httphost=$(dialog --backtitle "PMSRAPI" --title 'MicroService HTTP' --inputbox 'Enter the IP where this service will live' 7 60 "0.0.0.0"  --output-fd 1)
+        environtment=$(dialog --menu "PMSRAPI Environment" 20 45 35 dev "Development" test "Test" stage "Staging" prod "Production" --output-fd 1)
+        loglevel=$(dialog --menu "PMSRAPI Debug log level" 20 45 35 info "Information and Error" error "Only errors" none "No logs" prod "Production" --output-fd 1)
+        if dialog --title 'Database' --backtitle "PMSRAPI" --yesno "Is your service connected to MySQL or MariaDB?" 7 60; then
+            dbhost=$(dialog --backtitle "PMSRAPI" --title 'DataBase Setup' --inputbox 'Enter your DB Host' 7 60 "localost"  --output-fd 1)
+            dbname=$(dialog --backtitle "PMSRAPI" --title 'DataBase Setup' --inputbox 'Enter your DB Name' 7 60 --output-fd 1)
+            dbuser=$(dialog --backtitle "PMSRAPI" --title 'DataBase Setup' --inputbox 'Enter your DB User' 7 60 "root" --output-fd 1)
+            dbpass=$(dialog --backtitle "PMSRAPI" --title 'DataBase Setup' --passwordbox 'Enter your DB Password' 7 60 --output-fd 1)
+            dbport=$(dialog --backtitle "PMSRAPI" --title 'DataBase Setup' --inputbox 'Enter your DB Port' 7 60 "3306" --output-fd 1)
+            JSON_DATA="{
+    \"db\": {
+        \"host\": \"${dbhost}\",
+        \"port\": ${dbport},
+        \"name\": \"${dbname}\",
+        \"username\": \"${dbuser}\",
+        \"password\": \"${dbpass}\"
+    },
+    \"http\": {
+        \"port\": ${httpport},
+        \"host\": \"${httphost}\"
+    },
+    \"allowed_actions\": [
+        \"read\",
+        \"update\",
+        \"create\",
+        \"delete\"
+    ],
+    \"allowed_functions\":{
+        \"PUT\":[],
+        \"POST\":[],
+        \"GET\":[],
+        \"DELETE\":[]  
+    },
+    \"ms_logserver_token\":\"${mms_logservertoken}\",
+    \"ms_logserver_url\":\"${mms_logserver}\",
+    \"env\": \"${environtment}\",
+    \"local_log\": {
+        \"path\": \"/var/log/${mms_name}.log\",
+        \"level\": \"${loglevel}\"
+    }
+}"
+        else
+            JSON_DATA="{
+    \"http\": {
+        \"port\": ${httpport},
+        \"host\": \"${httphost}\"
+    },
+    \"allowed_actions\": [
+        \"read\",
+        \"update\",
+        \"create\",
+        \"delete\"
+    ],
+    \"allowed_functions\":{
+        \"PUT\":[],
+        \"POST\":[],
+        \"GET\":[],
+        \"DELETE\":[]  
+    },
+    \"ms_logserver_token\":\"${mms_logservertoken}\",
+    \"ms_logserver_url\":\"${mms_logserver}\",
+    \"env\": \"${environtment}\",
+    \"local_log\": {
+        \"path\": \"/var/log/${mms_name}.log\",
+        \"level\": \"${loglevel}\"
+    }
+}"
+        fi
+        echo "$JSON_DATA" > $mms_config_path
         dialog --title 'Config' --infobox "✅ Configuration moved to\n\n$mms_config_path\nYou can add your own payloads in this file" 7 60
         sleep 3
         if [ -z "$CRON_EXISTS" ]; then
@@ -75,8 +145,14 @@ define(\"ms_logserver\", \"${mms_logserver}\");
             sleep 3
         fi
         clear
-        echo "✅ Installation Completed, please remove the install.sh file, type:\n#~ rm install.sh\n\n"
-        echo "🚀 To start the service: type cd v1;php deploy.php"
+        echo "✅ Configuration Completed\n\n"
+        echo "🚀 Deploying service type cd v1;php deploy.php"
+        cd v1;php deploy.php
+        echo "🚀 Service Deployed"
+        echo "if you wish to expose this service to the internet, you can use Apache, NGINX reverse proxy or a service like ngrok"
+        echo "Your service is running at http://${httphost}:${httpport} try it now"
+        echo "by using curl http://${httphost}:${httpport}"
+        echo ""
     else
         clear
         echo "❌ Installation Cancelled $dialo_status"
