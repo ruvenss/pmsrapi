d32556ba45acd7ed76cd29e2144de672e463a99b
Ruvenss G. Wilches <ruvenss@gmail.com>
2025-02-08 01:38:00 +0100

    update

--- v1/GET/select_plot.php
+++ v1/GET/select_plot.php
@@ -6,3 +6,4 @@ function select_plot()
     $plot = sqlSelectPlot(request_data['parameters']['table'], $field_x, $field_y, request_data['parameters']['where'], request_data['parameters']['orderby'], request_data['parameters']['limit']);
     http_response(200, ["data" => ["rows" => $plot]]);
 }
+select_plot();
