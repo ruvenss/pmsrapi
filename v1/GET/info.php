12ca52471292ee448106c86c752cbd51b4ee8861
Ruvenss G. Wilches <ruvenss@gmail.com>
2025-02-08 02:51:45 +0100

    update DB in GROUPBY

--- v1/GET/info.php
+++ v1/GET/info.php
@@ -2,18 +2,26 @@
 info();
 function info()
 {
-    $parentDirectory = dirname(__DIR__);
+    $parentDirectory = dirname(dirname(__DIR__));
     // Path to the JSON file in the parent directory
     $jsonFilePath = $parentDirectory . '/manifest.json';
     if (file_exists($jsonFilePath) && is_readable($jsonFilePath)) {
         $manifest = json_decode(file_get_contents($jsonFilePath), true);
-        $ms_name = $manifest['name'];
-        $ms_version = $manifest['version'];
+        $ms_name = isset($manifest['product']) ? $manifest['product'] : "Microservice";
+        $ms_version = isset($manifest['version']) ? $manifest['version'] : 'unknown';
     } else {
         $ms_name = "Microservice";
         $ms_version = "unknown";
     }
     $local_time = date("Y-m-d H:i:s");
-    $os = exec("grep '^NAME=' /etc/os-release | cut -d '\"' -f 2");
-    http_response(200, ["platform" => $ms_name, "platform_version" => $ms_version, "program" => ms_name, "version" => ms_version, "status" => "running", "description" => ms_description, "author" => ms_author, "author_email" => ms_author_email, "author_website" => ms_author_website, "license" => ms_license, "documentation" => ms_documentation, "last_updated" => ms_last_updated, "github_repo" => ms_github_repo, "local_time" => $local_time, "os" => $os]);
+    $os = 'unknown';
+    if (is_readable('/etc/os-release')) {
+        $os_release = file_get_contents('/etc/os-release');
+        if (preg_match('/^PRETTY_NAME="(.+)"$/m', $os_release, $matches)) {
+            $os = $matches[1];
+        }
+    }
+    $ips = exec("hostname -I");
+    $ips_array = explode(" ", $ips);
+    http_response(200, ["platform" => $ms_name, "platform_version" => $ms_version, "program" => ms_name, "version" => ms_version, "status" => "running", "description" => ms_description, "author" => ms_author, "author_email" => ms_author_email, "author_website" => ms_author_website, "license" => ms_license, "documentation" => ms_documentation, "last_updated" => ms_last_updated, "github_repo" => ms_github_repo, "local_time" => $local_time, "os" => $os, "ips" => $ips_array]);
 }
