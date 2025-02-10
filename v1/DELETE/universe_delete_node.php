0879c19226ab720aec46f3a18017485435e49b85
Ruvenss G. Wilches <ruvenss@gmail.com>
2025-02-10 22:28:49 +0100

    0.0.12

---
+++ v1/DELETE/universe_delete_node.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * universe_delete_node.php
+ * Endpoint to delete a node in the universe
+ * DO NOT MODIFY THIS FILE.
+ * @author ruvenss <ruvenss@gmail.com>
+ */
+function universe_delete_node()
+{
+
+}
+universe_delete_node();
